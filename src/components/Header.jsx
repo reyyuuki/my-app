b5cfb8287ca272d4a70b2ed884a2a3731cf7b88a
Rey Yuuki <irfan31331@gmail.com>
2024-07-23 13:38:06 +0530

    adding data and icons or setting errors

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import {
   NavigationMenu,
   NavigationMenuContent,
@@ -9,56 +9,90 @@ import {
   NavigationMenuList,
   NavigationMenuTrigger,
 } from "@/components/ui/navigation-menu";
-import { FetchByData } from "./hooks/UseApiFetch";
+import { FetchByData, MangaDetail } from "./hooks/UseApiFetch";
 import Toggle from "./toggle";
-import { Input } from "@/components/ui/input";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { faBook, faFilm, faMagnifyingGlass, faShuffle, faXmark } from "@fortawesome/free-solid-svg-icons";
+import { Button } from "./ui/button";
 
 const Header = () => {
+  const [focused, setFocused] = useState(false);
+  const [data1, setData1] = useState('');
+  const [data2, setData2] = useState('');
+
   useEffect(() => {
     const fetching = async () => {
-      const data = await FetchByData(21);
+      const data = await FetchByData(16498);
       if (data) {
+        setData1(data);
         console.log(data);
+      }
+
+      const response = await MangaDetail(105398);
+      if(response){
+        setData2(response);
+        console.log(response);
       }
     };
     fetching();
   }, []);
+
+  const OnFocused = () => {
+    setFocused(true);
+  };
+
+  const Unfocused = () => {
+    setFocused(false);
+  };
+
+  if(!data1 || !data2) return <div>Hello</div>
+
   return (
-    <div className="flex w-full h-[65px] bg-zinc-800/30">
-      <div className="w-[18%] text-center bg-red-500">
-        <p className="text-[25px] ">
-          <span className="text-[45px] font-bold">A</span>nifusion
+    <div className="flex  items-center w-full h-[65px] bg-zinc-800/30">
+      <div className="w-[18%] text-center">
+        <p className="text-[25px]">
+          <span className="text-[45px] font-bold text-gray-600">A</span>nifusion
         </p>
       </div>
-      <div className=" flex w-[250px]  justify-center align-middle">
+      <div className=" flex justify-center items-start w-[400px] ">
         <NavigationMenu>
-          <NavigationMenuList>
-            <NavigationMenuItem>
-              <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
+          <NavigationMenuList className="gap-[10px]">
+            <NavigationMenuItem >
+              <NavigationMenuTrigger className="w-[150px] flex gap-[10px]">
+              <FontAwesomeIcon icon={faFilm} />
+                <p>Anime</p>
+                </NavigationMenuTrigger>
               <NavigationMenuContent>
                 <NavigationMenuLink>
                   <div className="flex w-[500px] h-[300px] p-[10px] bg-zinc-800/30">
                     <img
-                      src=""
+                      src={data1.image}
                       alt="Name"
                       className=" w-[200px] h-[100%] rounded-xl bg-blue-500"
                     />
+                    <div className="flex flex-col">
+                    <h1 className="text-[20px]">{data1.title && data1.title.english || data1.title.romaji || data1.title.native }</h1>
+                    </div>
                   </div>
                 </NavigationMenuLink>
               </NavigationMenuContent>
             </NavigationMenuItem>
             <NavigationMenuItem>
-              <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
+              <NavigationMenuTrigger className="w-[150px]  flex gap-[10px]">
+              <FontAwesomeIcon icon={faBook} />
+              <p>Manga</p>
+                </NavigationMenuTrigger>
               <NavigationMenuContent>
                 <NavigationMenuLink>
                   <div className="flex w-[500px] h-[300px] p-[10px] bg-zinc-800/30">
                     <img
-                      src=""
+                      src={data2.image}
                       alt="Name"
                       className=" w-[200px] h-[100%] rounded-xl bg-blue-500"
                     />
+                     <div className="">
+                    <h1>{data2.title && data2.title.english || data2.title.romaji}</h1>
+                    </div>
                   </div>
                 </NavigationMenuLink>
               </NavigationMenuContent>
@@ -66,13 +100,34 @@ const Header = () => {
           </NavigationMenuList>
         </NavigationMenu>
       </div>
-      <div className="flex justify-center items-center w-[600px] bg-black">
-        <div className="flex justify-center items-center">
-          <FontAwesomeIcon icon={faMagnifyingGlass} className="" />
-          <Input placeholder="Search Anime" className="w-[400px] px-[40px]" />
+      <div className="flex justify-center items-center w-[600px] ">
+        <div className="flex  justify-between items-center w-[500px] h-[35px] px-[10px] bg-zinc-800/30 Shadow rounded-[10px]">
+          <FontAwesomeIcon
+            icon={faMagnifyingGlass}
+            className={`text-[15px] transition duration-1000 ease ${focused ? "opacity-100" : "opacity-0"} `}
+          />
+          <input
+            placeholder="Search Anime"
+            onClick={() => OnFocused()}
+            className={`w-[300px] outline-none bg-transparent absolute transition duration-1000 ease ${
+              focused ? `translate-x-[30px]` : `translate-x-0`
+            }`}
+          />
+          <div className="flex relative">
+          <FontAwesomeIcon
+            icon={faMagnifyingGlass}
+            className={`text-[15px] transition duration-1000 ease ${focused ? "scale-0" : "scale-x-100"}`}
+          />
+          <FontAwesomeIcon
+            icon={faXmark}
+            className={`text-[15px] transition duration-1000 ease absolute ${focused ? "scale-100" : "scale-x-0"}`}
+            onClick={() => Unfocused()}
+          />
+          </div>
         </div>
       </div>
-      <div className="flex justify-center items-center">
+      <div className="flex justify-end items-center gap-[10px] text-xl h-[35px] w-[450px]">
+        <Button size={"icon"} variant={"outline"}><FontAwesomeIcon icon={faShuffle}/></Button>
         <Toggle />
       </div>
     </div>
